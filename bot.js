b29db6757d3414c074b698946e7ac90a998d2727
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-07-26 12:52:41 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,7 +86,7 @@ async function whatsAsena () {
     
     const conn = new WAConnection();
     const Session = new StringSession();
-    conn.version = [2,2121,7];
+    conn.version = [2,2123,8];
 
     conn.logger.level = config.DEBUG ? 'debug' : 'warn';
     var nodb;
